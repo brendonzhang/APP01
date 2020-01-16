4836a8321f6a82047fb8d3016328f6333d70ad8e
brendonzhang <57387538+brendonzhang@users.noreply.github.com>
2020-01-16 13:00:16 +0800

    scaleMode:fill
    修正圖片問題

--- src/books/HeartSutra.js
+++ src/books/HeartSutra.js
@@ -25,7 +25,7 @@ module.exports = class HeartSutra {
 			.onSelect( () => this.hideWindow() )
 			.appendTo(this.el);
 			
-		this.bgpicture = new ImageView({left: 0, top: 0, image: this.diaplayBgPicture})
+		this.bgpicture = new ImageView({left: 0, top: 0, scaleMode:fill, image: this.diaplayBgPicture})
 			.appendTo(this.el);
 		
 		this.bgbuler = new Composite({left: 0, top: 0, right: 0, bottom: 0,background: 'rgba(0, 0, 0, 0.6)'})
